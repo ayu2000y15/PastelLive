e0d8576cd39de2faccc51f276ab51a62ab68e6f8
ayu2000y15 <ayu2000y15@nasystem.net>
2025-04-02 11:23:49 +0900

    　　日付：2025/04/02 　変更者：野口 変更内容：ABOUTタイトル画像に変更

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
     <div class="container about">
-        <h1 class="page-title">ABOUT</h1>
-        {{-- <img src="{{ asset($titleAbout->file_path . $titleAbout->file_name) }}" alt="タイトル" class="title-image"> --}}
+        {{-- <h1 class="page-title">ABOUT</h1> --}}
+        <img src="{{ asset($titleAbout->file_path . $titleAbout->file_name) }}" alt="タイトル" class="title-image">
 
         <div class="about-container">
             <div class="about-logo">
